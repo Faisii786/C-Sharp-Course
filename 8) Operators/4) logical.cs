38f6eb2653dfdea56accec738a95f18d18f60dec
Faisal Aslam <f.aslam@irc.sa>
2026-02-08 16:36:43 +0300

    C# Course

---
+++ 8) Operators/4) logical.cs
@@ -0,0 +1,10 @@
+// Logical Operators
+// As with comparison operators, you can also test for True or False values with logical operators.
+
+// Logical operators are used to determine the logic between variables or values:
+
+// Operator	Name	Description	Example	Try it
+// && 	Logical and	Returns True if both statements are true	x < 5 &&  x < 10	
+// || 	Logical or	Returns True if one of the statements is true	x < 5 || x < 4	
+// !	Logical not	Reverse the result, returns False if the result is true	!(x < 5 && x < 10)	
+// You will learn more about comparison and logical operators in the Booleans and If...Else chapters.
---
+++ 9. math.cs
@@ -0,0 +1,32 @@
+// The C# Math class has many methods that allows you to perform mathematical tasks on numbers.
+
+// Math.Max(x,y)
+// The Math.Max(x,y) method can be used to find the highest value of x and y:
+
+// Example
+Math.Max(5, 10);
+
+// Math.Min(x,y)
+// The Math.Min(x,y) method can be used to find the lowest value of of x and y:
+
+// Example
+Math.Min(5, 10);
+
+// Math.Sqrt(x)
+// The Math.Sqrt(x) method returns the square root of x:
+
+// Example
+Math.Sqrt(64);
+
+// Math.Abs(x)
+// The Math.Abs(x) method returns the absolute (positive) value of x:
+
+// Example
+Math.Abs(-4.7);
+
+// Math.Round()
+// Math.Round() rounds a number to the nearest whole number:
+
+// Example
+Math.Round(9.99);
+
